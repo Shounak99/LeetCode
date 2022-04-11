755b855b3b24df71970b6af6a1094307572f1a98
Shounak99 <shounakkossambe@gmail.com>
2022-04-11 12:42:44 +0530

    Commited Path Sum from root to leaf

---
+++ BinaryTreeQuestions/PathSum.java
@@ -0,0 +1,29 @@
+package BinaryTreeQuestions;
+class TreeNode{
+    int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+}
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null){
+            return false;
+        }
+        if(root.left==null && root.right==null && targetSum-root.val==0){
+            return true;
+        }
+        else if(root.left==null && root.right==null && targetSum-root.val!=0){
+            return false;
+        }
+        boolean left=hasPathSum(root.left,targetSum-root.val);
+        boolean right=hasPathSum(root.right,targetSum-root.val);
+        return left||right;
+    }
+}

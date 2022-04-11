e90aac4e4cf29b7937e5e03d0431da0cda8f7eec
Shounak99 <shounakkossambe@gmail.com>
2022-04-11 10:40:24 +0530

    Commited Max Depth Of Binary Tree

---
+++ BinaryTreeQuestions/MaxDepthOfTree.java
@@ -0,0 +1,28 @@
+package BinaryTreeQuestions;
+
+
+
+public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+    
+public class MaxDepthOfTree {
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int left_depth=maxDepth(root.left);
+        int right_depth=maxDepth(root.right);
+        return Math.max(left_depth,right_depth)+1;
+        
+    }
+}

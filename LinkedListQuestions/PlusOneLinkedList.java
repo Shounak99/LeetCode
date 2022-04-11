33043ba3c8507d2635f78be4c7c076540c0f02e5
Shounak99 <shounakkossambe@gmail.com>
2022-04-11 16:22:19 +0530

    Commited PlusOne LinkedList Question

---
+++ LinkedListQuestions/PlusOneLinkedList.java
@@ -0,0 +1,43 @@
+//https://leetcode.com/problems/plus-one-linked-list/
+package LinkedListQuestions;
+class ListNode{
+    int val;
+    ListNode next;
+    public ListNode(){
+
+    }
+    public ListNode(int val){
+        this.val=val;
+    }
+    public ListNode(int val,ListNode next){
+        this.val=val;
+        this.next=next;
+    }
+
+}
+public class PlusOneLinkedList {
+    public ListNode plusOne(ListNode head) {
+        ListNode sentinel=new ListNode(0);
+        ListNode notNine=sentinel;
+        sentinel.next=head;
+        while(head!=null ){
+            if(head.val!=9){
+                notNine=head;
+            }
+            head=head.next;
+        }
+        notNine.val++;
+        notNine=notNine.next;
+        while(notNine!=null){
+            notNine.val=0;
+            notNine=notNine.next;
+        }
+        if(sentinel.val==0){
+            return sentinel.next;
+        }
+        return sentinel;
+
+        
+    }
+}
+}

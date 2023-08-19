41f98d23d6154dec996ab5edd8c343eead0e1e07
KOSSAMBE <shounak-nandan.kossambe@capgemini.com>
2023-08-19 20:54:20 +0530

    Committed Leetcode 53 Maximum Subarray

--- Arrays_1_One_Dimensional/MaxSumContiguousSubarray.java
+++ Arrays_1_One_Dimensional/MaxSumContiguousSubarray.java
@@ -1,11 +1,10 @@
-public class Solution {
-    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
-    public int maxSubArray(final int[] A) {
+class Solution {
+    public int maxSubArray(int[] nums) {
         int sum=0;
         int maxSum=Integer.MIN_VALUE;
-        for(int i=0;i<A.length;i++){
-            sum+=A[i];
-            maxSum=Math.max(sum,maxSum);
+        for(int num:nums){
+            sum+=num;
+            maxSum=Math.max(maxSum,sum);
             if(sum<0){
                 sum=0;
             }
